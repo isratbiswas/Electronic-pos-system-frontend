b19aaa69ca6c50739c3f23d871fd9030f7d29f28
isratbiswas <isratbiswas28@gmail.com>
2025-12-10 18:50:00 +0600

    daily sales management

--- src/app/(dashboardLayout)/manager/dailySales/page.tsx
+++ src/app/(dashboardLayout)/manager/dailySales/page.tsx
@@ -1,16 +1,16 @@
-// import DailySale from "@/components/modules/sales/DailySale";
-// import { getDailySales } from "@/services/admin/salesReport";
+import DailySale from "@/components/modules/sales/DailySale";
+import { getDailySales } from "@/services/admin/salesReport";
 
 
-// const DailySalePage = async() => {
-//     const dailySalesProduct = await getDailySales()
-//     const dailySales = dailySalesProduct?.data ?? []
-//     console.log(dailySalesProduct, 'daily');
-//     return (
-//         <div>
-//             <DailySale dailySales={dailySales}/>
-//         </div>
-//     );
-// };
+const DailySalePage = async() => {
+    const dailySalesProduct = await getDailySales()
+    const dailySales = dailySalesProduct?.data ?? []
+    console.log(dailySalesProduct, 'daily');
+    return (
+        <div>
+            <DailySale dailySales={dailySales}/>
+        </div>
+    );
+};
 
-// export default DailySalePage;
+export default DailySalePage;
